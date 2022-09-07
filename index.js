0aebc0fc7cfbe75bb6cc8b89a67c1c6115daab18
007vict <super_vg@bk.ru>
2022-09-07 19:31:39 +0300

    docs: catch errors

--- index.js
+++ index.js
@@ -25,25 +25,45 @@ app.get('/',(req, res) => {
 })
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign({
+      _id: user._id
+    },
+    "secret123",
+    {
+      expiresIn: "30d",
+    }
+    )
+
+    return res.json({
+      ...user._doc,
+      token
+    });
   }
-
-  const password = req.body.password
-  const salt = await bcrypt.genSalt(10)
-  const passwordHash = await bcrypt.hash(password, salt)
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  })
-
-  const user = await doc.save()
-
-  return res.json(user)
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message : "Failed to register",
+    })
+  }
 })
 
 app.listen(4444, (err) => {
--- validations/auth.js
+++ validations/auth.js
@@ -1,7 +1,7 @@
 import { body } from "express-validator"
 
 export const registerValidation = [
-  body('email').isEmail(),
+  body('email', 'неправильный пароль').isEmail(),
   body('password').isLength({ min: 5 }),
   body('fullName').isLength({ min: 3 }),
   body('avatarUrl').optional().isURL(),
